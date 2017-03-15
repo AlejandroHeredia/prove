b2bae8ae5c5ca85c2d910346cbb6980ea572c03b
Alejandro Heredia <Alejandro.Heredia@fundacion-jala.org>
2017-03-15 17:49:46 -0400

    add for

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    int a=45;
+    for(auto i=0; i<=a;i++)
+    {
+        cout<<"mi branch"<<endl;
+    }
     return 0;
 }
